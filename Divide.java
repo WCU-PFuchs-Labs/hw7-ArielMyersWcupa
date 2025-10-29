89d0930ae42ee894682f561fbc0098939685b505
ArielMyersWcupa <AM1044820@wcupa.edu>
2025-10-28 22:00:30 -0400

    Update Divide.java

--- Divide.java
+++ Divide.java
@@ -4,11 +4,14 @@ import java.util.Random;
 * Purpose: A binary operator for Division
 */
 public class Divide extends Binop {
-    public double eval(double[] data) {
-        double denom = right.eval(data);
-        if (Math.abs(denom) < 0.0001)
+    
+    public double eval(double a, double b) {
+        if (Math.abs(b) < 0.0001) {
             return 1.0; 
-        return left.eval(data) / denom;
+        }
+        return a / b;
     }
-    public String toString() { return "/"; }
+    public String toString() { 
+        return "/";
+    }
 }
