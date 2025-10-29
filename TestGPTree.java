c4c55457207608391e4c87571e3dd0d642fe718f
ArielMyersWcupa <AM1044820@wcupa.edu>
2025-10-28 21:17:03 -0400

    Create TestGPTree.java

---
+++ TestGPTree.java
@@ -0,0 +1,28 @@
+import java.util.*;
+/*
+* Code Author: Ariel Myers
+*/
+
+public class TestGPTree {
+    public static void main(String[] args) {
+        Random rand = new Random();
+        int maxDepth = 4;
+        int numVars = 3;
+        double[] inputs = {3.14, 2.78, 1.0};
+
+        Binop[] ops = {new Plus(), new Minus(), new Mult(), new Divide()};
+        NodeFactory factory = new NodeFactory(ops, numVars);
+
+        GPTree treeA = new GPTree(factory, maxDepth, rand);
+        GPTree treeB = new GPTree(factory, maxDepth, rand);
+
+        System.out.println("Tree A: " + treeA + " = " + treeA.eval(inputs));
+        System.out.println("Tree B: " + treeB + " = " + treeB.eval(inputs));
+
+        treeA.crossover(treeB, rand);
+
+        System.out.println(“\n--- After the Crossover ---”);
+        System.out.println("Tree A: " + treeA + " = " + treeA.eval(inputs));
+        System.out.println("Tree B: " + treeB + " = " + treeB.eval(inputs));
+    }
+}
