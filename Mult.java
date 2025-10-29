1fbdc57877c87f7119b764733fe480cac2b4a6c7
ArielMyersWcupa <AM1044820@wcupa.edu>
2025-10-28 21:22:47 -0400

    Update Mult.java

--- Mult.java
+++ Mult.java
@@ -3,7 +3,7 @@ import java.util.Random;
 * Code Author: Ariel Myers
 * Purpose: A binary operator for Subtraction
 */
-public class Minus extends Binop {
-    public double eval(double left, double right) { return left - right; }
-    public String toString() { return "-"; }
+public class Mult extends Binop {
+    public double eval(double left, double right) { return left * right; }
+    public String toString() { return "*"; }
 }
