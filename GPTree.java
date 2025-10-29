acc3c7987aa99754ca3bfb9031a8810fdb48bd75
ArielMyersWcupa <AM1044820@wcupa.edu>
2025-10-28 23:14:18 -0400

    Update GPTree.java

--- GPTree.java
+++ GPTree.java
@@ -7,49 +7,63 @@ public class GPTree implements Collector {
     private Node root;
     private ArrayList<Node> crossNodes;
 
-  GPTree() {
-     root = null;
-  }
-
-  public GPTree(NodeFactory nf, int maxDepth, Random rand) { //For the constructor to build a tree randomly//
-      root = nf.getOperator(rand);
-      root.addRandomKids(nf, maxDepth, rand);
-  }
-  public void collect(Node node) {
-    if (node.getOp() instanceof Binop) {
-        crossNodes.add(node);
-    }
-  }
-    
-  public void traverse() {
-      crossNodes = new ArrayList<>(); //To create a list and traverse it//
-      root.traverse(this);
+    //Constructor//
+    GPTree() {
+        root = null;
+        crossNodes = new ArrayList<>();
     }
 
-    
-  public String getCrossNodes() {
-      StringJoiner sj = new StringJoiner(";");
-      for (Node n : crossNodes) sj.add(n.toString()); //String of nodes that are separated by semicolons//
-      return sj.toString();
-  }
+    // Constructor that will randomly build a tree//
+    public GPTree(NodeFactory nf, int maxDepth, Random rand) {
+        root = nf.getOperator(rand);
+        root.addRandomKids(nf, maxDepth, rand);
+        crossNodes = new ArrayList<>();
+    }
 
-    
-  public void crossover(GPTree other, Random rand) { //This is to swap random subtrees within the GPTrees//
+    //Called by Node.traverse() for each node//
+    @Override
+    public void collect(Node node) {
+        if (node != null && node.getOp() instanceof Binop) {
+            crossNodes.add(node);
+        }
+    }
+
+    //Begins a traversal and collects all Binops//
+    public void traverse() {
+        if (root == null) return;
+        // Always clear before collecting new nodes
+        crossNodes.clear();
+        root.traverse(this);
+    }
+
+    //Returns a string of collected nodes separated by semicolons//
+    public String getCrossNodes() {
+        StringJoiner sj = new StringJoiner(";");
+        for (Node n : crossNodes) {
+            sj.add(n.toString());
+        }
+        return sj.toString();
+    }
+
+    //Subtree crossover//
+    public void crossover(GPTree other, Random rand) {
         this.traverse();
         other.traverse();
+
+        if (crossNodes.isEmpty() || other.crossNodes.isEmpty()) return;
 
         Node n1 = crossNodes.get(rand.nextInt(crossNodes.size()));
         Node n2 = other.crossNodes.get(rand.nextInt(other.crossNodes.size()));
 
         if (rand.nextBoolean()) n1.swapLeft(n2);
         else n1.swapRight(n2);
-  }
+    }
 
-  public String toString() {
-      return root.toString();
-  }
+    public String toString() {
+        return (root == null) ? "" : root.toString();
+    }
 
-  public double eval(double[] data) {
-      return root.eval(data);
-  }
+    public double eval(double[] data) {
+        return root.eval(data);
+    }
 }
