8c7354406479dc3c39159093b6bd5eb945444f91
ArielMyersWcupa <AM1044820@wcupa.edu>
2025-10-28 20:59:22 -0400

    Update GPTree.java

--- GPTree.java
+++ GPTree.java
@@ -15,3 +15,41 @@ public class GPTree implements Collector {
       root = nf.getOperator(rand);
       root.addRandomKids(nf, maxDepth, Random rand) {
   }
+
+  public void collect(Node n) {
+        if (!n.isLeaf()) crossNodes.add(n); //Only collect nodes that are specifically NOT leaves//
+    }
+
+    
+    public void traverse() {
+        crossNodes = new ArrayList<>(); //To create a list and traverse it//
+        root.traverse(this);
+    }
+
+    
+    public String getCrossNodes() {
+        StringJoiner sj = new StringJoiner(";");
+        for (Node n : crossNodes) sj.add(n.toString()); //String of nodes that are separated by semicolons//
+        return sj.toString();
+    }
+
+    
+    public void crossover(GPTree other, Random rand) { //This is to swap random subtrees within the GPTrees//
+        this.traverse();
+        other.traverse();
+
+        Node n1 = crossNodes.get(rand.nextInt(crossNodes.size()));
+        Node n2 = other.crossNodes.get(rand.nextInt(other.crossNodes.size()));
+
+        if (rand.nextBoolean()) n1.swapLeft(n2);
+        else n1.swapRight(n2);
+    }
+
+    public String toString() {
+        return root.toString();
+    }
+
+    public double eval(double[] data) {
+        return root.eval(data);
+    }
+}
